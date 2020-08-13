2cdf18e8706f67c00b2fdb00de79c38eb82bfbd2
Norman Johnson <norman-johnson@lambdastudents.com>
2020-08-13 10:51:44 -0500

    more tasks complete

--- index.js
+++ index.js
@@ -39,8 +39,8 @@ Your function should return a boolean TRUE if the length of the array is 31 and 
 To save you from having to count the items above, you can assume that length of originalFlavors is 31. You may also want to test your function by creating and passing an array of a different length, to make sure it returns FALSE if length is not 31. 
 
 i.e. is31Flavors(originalFlavors) will return TRUE.*/
-
 console.log('********************* TASK 1 ******************')
+
 function is31Flavors(arr){
     // console.log(arr.length)
 if (arr.length === 31){
@@ -66,17 +66,17 @@ Your function should accept:
 Your function should add the flavor to the front of the array and console.log the resulting array.
 
 For example addFlavor("Rainbow Sherbert", originalFlavors) should return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla Burnt Almond"] */ 
-
 console.log('********************* TASK 2 ******************')
+
 function addFlavor(arr, newFlavor){
-let newArr = [newFlavor, ...arr];
-// newArr.unshift(newFlavor);
-console.log(newArr);
-return newArr;
+let newArr = [newFlavor, ...arr]
+ newArr.unshift(newFlavor)
+console.log(newArr)
+return newArr
 
 }
 addFlavor(originalFlavors, 'Rainbow Sherbert')
-;
+
 /* Task 3: Houston, we have a problem! There are now 32 flavors in the array! Your task is to remove an item from the end of the array. 
 
 Your function should accept:
@@ -86,14 +86,18 @@ Your function should accept:
 Your function should remove a flavor from the end of the array and console.log the resulting array.
 
 For example removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]*/ 
+console.log('********************* TASK 3 ******************')
 
-function removeLastFlavor(/*code here*/){
-
-    /*code here*/
-
+function removeLastFlavor(arr, lastFlavor){
+let newArr = [...arr, lastFlavor];
+newArr.pop(lastFlavor);
+console.log(newArr);
+return newArr;
 }
-
-/* Task 4: Write a function that returns a flavor at a given index in the array.
+console.log(removeLastFlavor, 'Vanilla Burnt Almond')
+    
+    
+    /* Task 4: Write a function that returns a flavor at a given index in the array.
 
 Your function should accept:
 
@@ -101,13 +105,14 @@ Your function should accept:
 (2) an index
 
 For example, getFlavorByIndex(originalFlavors, 2) would return "Black Walnut", assuming Rainbow Sherbert has been added successfully. */
+console.log('********************* TASK 4 ******************')
 
-function getFlavorByIndex(/*code here*/){
-
-    /*code here*/
+function getFlavorByIndex(arr){
+    let newArr=[...arr]
+return newArr
 
 }
-
+console.log(getFlavorByIndex, 'Coffee')
 /* Task 5: As corporate wants to add more and more flavors to their lineup, they've realized that they need to remove flavors based on flavor name, as opposed to just arbitrarily removing the first or last flavor. Your task is to get an index by flavor name, and remove that flavor from the array. 
 
 Your function should accept: 
@@ -120,13 +125,13 @@ For example, removeFlavorByName(originalFlavors, "Vanilla") would return an arra
 Hint: You can use .splice() for this
 
 */
+console.log('********************* TASK 5 ******************')
 
-function removeFlavorByName(/*code here*/){
-
-    /*code here*/
-
+function removeFlavorByName(arr, string){
+let newArr = originalFlavor.splice(17,3)
 }
-
+console.log(removeFlavorByName) 
+// console.log(removeLastFlavor(originalFlavors, 'Lemon'))
 
 /* Task 6: With all of these changes going on, we don't want to lose track of the actual, original 31 flavors. Write a function called copy that makes a copy of the array. 
 
@@ -135,10 +140,11 @@ Your function should accept:
 2 arguments 1 for your new array and one for your original array
 
 and should return a new array that is identical to the old array. You can name the new array however you'd like. */
+console.log('********************* TASK 6 ******************')
 
-function copy(/*code here*/){
+function copy(){
 
-    /*code here*/
+  
 
 }
 
@@ -156,15 +162,21 @@ For example, filterByWord(originalFlavors, "Chocolate") should return ["Chocolat
 DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem.
 
 hint - you can use the .includes method to help you solve this */
+console.log('********************* TASK 7 ******************')
 
-function filterByWord(/*code here*/){
-
-    /*code here*/
+function filterByWord(arr, string){
+let newArr = []
+for (let i = 0; i< arr.length; i++){
+    if(arr[i].includes(string)){
+newArr.push(arr[i])
+    }
+}
+    return newArr
 
 }
 
 
-
+console.log(filterByWord(originalFlavors, 'Lemon'))
 /* 🧁🍦🍨 STRETCH 🍨🍦🍫*/ 
 
 /* STRETCH 1: Write a function that returns the average number of words in an array. You should be able to use this function for any array, but can test with originalFlavors.
